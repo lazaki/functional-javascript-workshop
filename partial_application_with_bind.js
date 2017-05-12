88d4cf6d42614bb5f2811e51d9cfd677d4790c10
lazaki <lazar.rk@gmail.com>
2017-05-12 11:56:57 +0200

    with_bind

--- partial_application_with_bind.js
+++ partial_application_with_bind.js
@@ -1,10 +1,3 @@
-var slice = Array.prototype.slice
-
-function logger(namespace) {
-    return function() {
-        var message = slice.call(arguments).join(' ')
-        console.log(namespace + " " + message);
+ module.exports = function(namespace) {
+      // SOLUTION GOES HERE
     }
-}
-
-module.exports = logger
